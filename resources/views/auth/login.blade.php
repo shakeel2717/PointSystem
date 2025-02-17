9e94e55b5cba58b4dec7e92d7a730210fbc1bd3d
Shakeel Ahmad <shakeel2717@gmail.com>
2025-02-17 20:14:29 +0500

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+    <div class="my-4">
+        <img src="{{ asset('logo-vr.png') }}" alt="Logo">
+    </div>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <div class="form-floating mb-3 mt-3">
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+            <label for="email">Email</label>
+        </div>
+
+        <div class="form-floating mb-3 mt-3">
+            <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+            <label for="password">Password</label>
+        </div>
+
+        <div class="my-4">
+            <button type="submit" class="btn btn-warning text-white btn-lg">Sign In</button>
+        </div>
+        <div class="text-center">
+            <a href="{{ route('register') }}" class="btn text-muted">Don't have an Account?</a>
+        </div>
+    </form>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+    <div class="my-4">
+        <img src="{{ asset('logo-vr.png') }}" alt="Logo">
+    </div>
+    <form action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="form-floating mb-3 mt-3">
+            <input type="text" class="form-control" id="name" placeholder="Enter Full Name" name="name">
+            <label for="name">Full Name</label>
+        </div>
+
+        <div class="form-floating mb-3 mt-3">
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+            <label for="email">Email</label>
+        </div>
+
+        <div class="form-floating mb-3 mt-3">
+            <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+            <label for="password">Password</label>
+        </div>
+
+        <div class="form-floating mb-3 mt-3">
+            <input type="password" class="form-control" id="password_confirmation" placeholder="Enter password"
+                name="password_confirmation">
+            <label for="password_confirmation">Confirm Password</label>
+        </div>
+
+        <div class="my-4">
+            <button type="submit" class="btn btn-warning text-white btn-lg">Create Account</button>
+        </div>
+        <div class="text-center">
+            <a href="{{ route('login') }}" class="btn text-muted">Already have an Account?</a>
+        </div>
+    </form>
+@endsection
