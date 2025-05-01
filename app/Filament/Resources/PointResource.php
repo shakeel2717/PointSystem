9277ec649e4b3dbdac6dae29bbcfcd3813aabbbe
Shakeel Ahmad <shakeel2717@gmail.com>
2025-05-01 10:51:20 -0700

    Admin can Add Remove Points

---
+++ app/Filament/Resources/PointResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PointResource\Pages;
+use App\Filament\Resources\PointResource\RelationManagers;
+use App\Models\Point;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PointResource extends Resource
+{
+    protected static ?string $model = Point::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')
+                    ->label('User')
+                    ->required()
+                    ->searchable()
+                    ->options(User::pluck('phone', 'id')),
+
+                Forms\Components\TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('sum')
+                    ->required(),
+                Forms\Components\Textarea::make('reference')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('sum')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPoints::route('/'),
+            'create' => Pages\CreatePoint::route('/create'),
+            'edit' => Pages\EditPoint::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Policies/PointPolicy.php
+++ app/Policies/PointPolicy.php
@@ -13,7 +13,7 @@ class PointPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class PointPolicy
      */
     public function view(User $user, Point $point): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class PointPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class PointPolicy
      */
     public function update(User $user, Point $point): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class PointPolicy
      */
     public function delete(User $user, Point $point): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class PointPolicy
      */
     public function restore(User $user, Point $point): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class PointPolicy
      */
     public function forceDelete(User $user, Point $point): bool
     {
-        return false;
+        return true;
     }
 }
